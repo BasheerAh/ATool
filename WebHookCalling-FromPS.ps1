4ea7f5125211f25eabf4775a36cd32799525e99a
BasheerAh <basheer@live.com.sg>
2017-08-13 15:56:09 +0530

    added webhook example

---
+++ WebHookCalling-FromPS.ps1
@@ -0,0 +1,11 @@
+﻿$uri = "https://s1events.azure-automation.net/webhooks?token=BUsHuJeh8mJ8gBArl2%2bByqUdU7xqBUeOldXmr%2f6w7u8%3d"
+$headers = @{"From"="iass.lab@outlook.com";"Date"="08/13/2017 15:38:00"}
+
+$vms  = @(
+            @{ Name="vm01";ServiceName="vm01"},
+            @{ Name="vm02";ServiceName="vm02"}
+        )
+$body = ConvertTo-Json -InputObject $vms
+
+$response = Invoke-RestMethod -Method Post -Uri $uri -Headers $headers -Body $body
+$jobid = ConvertFrom-Json $response
