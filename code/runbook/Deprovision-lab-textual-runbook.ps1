7e9d1643d21b8ab493fafd1ed32835ebd10b4c6a
BasheerAh <basheer@live.com.sg>
2017-08-13 13:11:39 +0530

    Created Deprovision-lab-textual-runbook.ps1 in AzureAutomation.

---
+++ code/runbook/Deprovision-lab-textual-runbook.ps1
@@ -0,0 +1,6 @@
+workflow Deprovision-lab-textual-runbook {
+$c = Get-AutomationConnection -Name 'AzureRunAsConnection'
+Add-AzureRmAccount -ServicePrincipal -Tenant $c.TenantID -ApplicationID $c.ApplicationID -CertificateThumbprint $c.CertificateThumbprint
+$resourceGroupName = Get-AutomationVariable -Name 'ResourceGroupName'
+Remove-AzureRmResourceGroup -Name $resourceGroupName -Force
+}
